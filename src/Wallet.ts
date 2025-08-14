30a817ff06b38c6fd8707a09208237adcbcf66b6
Vladimir Sinyakov <vladimir.siniakov@gmail.com>
2025-08-14 13:05:17 +0200

    Remove mnemonic wallet support

--- src/Wallet.ts
+++ src/Wallet.ts
@@ -1,53 +1,40 @@
 import * as CSL from '@emurgo/cardano-serialization-lib-browser';
-import * as bip39 from '@scure/bip39';
-import { wordlist } from '@scure/bip39/wordlists/english';
 import { Backend } from './Backend';
 import { UTxO, Output, BigIntWrap } from './Types';
 import { Prover } from './Prover';
-import { BufferUtils, hexToBytes } from './Utils';
+import { hexToBytes } from './Utils';
 
 /**
- * Supported Cardano networks
+ * We support Bech32 addresses and Gmail-locked smart contracts.
  */
-export type Network = 'preview' | 'preprod' | 'mainnet';
-
-/**
- * Whether the wallet was initialised with a mnemonic or with Gmail.
- */
-export enum WalletType {
-    Mnemonic = 0,
+export enum AddressType {
+    Bech32 = 0,
     Google = 1
 }
 
 /**
  * Data required to initialise a wallet.
  * 
- * When method is WalletType.Mnemonic:
- *  data is the mnemonic
- *  rootKey is not required
- *
- * When method is WalletType.Google:
  *  data is Google JSON Web Token as a string
  *  rootKey is the private key to sign transactions (can be generated randomly)
  */
 export interface WalletInitialiser {
-    method: WalletType;
     data: string;
     rootKey?: string;
 }
 
 /**
  * Describes the recipient of ADA
- * @property {WalletType} recipientType  - Type of wallet the recipient holds
- * @property {string} address            - Cardano address if recipientType is Mnemonic, email otherwise
+ * @property {AddressType} recipientType  - Type of wallet the recipient holds
+ * @property {string} address            - Cardano address if recipientType is Bech32, email otherwise
  * @property {Asset} assets              - A dictionary of assets to send. For ADA, use 'lovelace' as the key. For other assets, use the format '<PolicyID>.<AssetName>'
  */
 export class SmartTxRecipient {
-    recipientType: WalletType;
+    recipientType: AddressType;
     address: string;
     assets: Asset;
 
-    constructor(recipientType: WalletType, address: string, assets: Asset) {
+    constructor(recipientType: AddressType, address: string, assets: Asset) {
         this.recipientType = recipientType;
         this.address = address;
         this.assets = assets;
@@ -62,91 +49,46 @@ export interface Asset {
 }
 
 /**
- * The Wallet which can be initialised with a mnemonic or a email address.
+ * The Wallet which can be initialised with an email address.
  */
 export class Wallet {
-    private rootKey!: CSL.Bip32PrivateKey;    // Only for Mnemonic
-    private accountPrvKey!: CSL.Bip32PrivateKey; // Only for Mnemonic
-    private accountPubKey!: CSL.Bip32PublicKey;  // Only for Mnemonic
-    private stakeKey!: CSL.Bip32PublicKey;    // Only for Mnemonic
-
-    private tokenSKey!: CSL.Bip32PrivateKey;  // Only for Google
-    private jwt!: string;        // Only for Google
-    private userId!: string;     // Only for Google
+    private tokenSKey!: CSL.Bip32PrivateKey;
+    private jwt!: string;
+    private userId!: string;
     private freshKey: boolean = false;
 
     private backend: Backend;
     private prover: Prover;
-    private method: WalletType;
-    private network: Network;
 
     /**
-     *  @param {Backend} backend         - A Backend object for communication with Cardano
+     *  @param {Prover} prover                 - A Prover object for interaction with the prover
+     *  @param {Backend} backend               - A Backend object for interaction with the backend
      *  @param {WalletInitialiser} initialiser - Data to initialise the wallet
-     *  @param {string} password         - Optional password
-     *  @param {Network} network         - Accepted values: 'mainnet', 'preprod', 'preview'
-     *  @param {WalletOptions} options   - Browser/extension compatibility options
      */
-    constructor(backend: Backend, prover: Prover, initialiser: WalletInitialiser, password: string = '', network: Network = 'mainnet') {
+    constructor(backend: Backend, prover: Prover, initialiser: WalletInitialiser) {
         this.backend = backend;
         this.prover = prover;
-        this.network = network;
-        this.method = initialiser.method;
 
-        if (this.method == WalletType.Mnemonic) {
-            const entropy = bip39.mnemonicToEntropy(initialiser.data, wordlist);
-            this.rootKey = CSL.Bip32PrivateKey.from_bip39_entropy(
-                BufferUtils.from(entropy, 'hex'),
-                BufferUtils.from(password),
-            );
-            this.deriveKeys();
+        // At this point, we assume that userId is a valid email accessible by the user (i.e. the user was able to complete Google authentication).
+        this.jwt = initialiser.data;
+
+        const parts = this.jwt.split(".");
+        const payload = JSON.parse(atob(parts[1].replace(/-/g, '+').replace(/_/g, '/')));
+        this.userId = payload.email;
+
+        if (!initialiser.rootKey) {
+            const prvKey = CSL.Bip32PrivateKey
+                .generate_ed25519_bip32()
+                .derive(harden(1852)) // purpose
+                .derive(harden(1815)) // coin type
+                .derive(harden(0)) // account #0
+                .derive(0)
+                .derive(0);
+            this.tokenSKey = prvKey;
+            this.freshKey = true;
         } else {
-            // At this point, we assume that userId is a valid email accessible by the user (i.e. the user was able to complete Google authentication).
-            this.jwt = initialiser.data;
-
-            const parts = this.jwt.split(".");
-            const payload = JSON.parse(atob(parts[1].replace(/-/g, '+').replace(/_/g, '/')));
-            this.userId = payload.email;
-
-            if (!initialiser.rootKey) {
-                const prvKey = CSL.Bip32PrivateKey
-                    .generate_ed25519_bip32()
-                    .derive(harden(1852)) // purpose
-                    .derive(harden(1815)) // coin type
-                    .derive(harden(0)) // account #0
-                    .derive(0)
-                    .derive(0);
-                this.tokenSKey = prvKey;
-                this.freshKey = true;
-            } else {
-                this.tokenSKey = CSL.Bip32PrivateKey.from_hex(initialiser.rootKey);
-            }
+            this.tokenSKey = CSL.Bip32PrivateKey.from_hex(initialiser.rootKey);
         }
-    }
-
-    /**
-     * For a mnemonic-based wallet, derive all necessary keys
-     *
-     * Adapted from https://developers.cardano.org/docs/get-started/cardano-serialization-lib/generating-keys/
-     */
-    private deriveKeys(): void {
-        if (this.method == WalletType.Google) {
-            return;
-        }
-        this.accountPrvKey = this.rootKey
-            .derive(harden(1852)) // purpose
-            .derive(harden(1815)) // coin type
-            .derive(harden(0)); // account #0
-
-        this.accountPubKey = this.accountPrvKey
-            .derive(0) // external
-            .derive(0)
-            .to_public();
-
-        this.stakeKey = this.accountPrvKey
-            .derive(2) // chimeric
-            .derive(0)
-            .to_public();
     }
 
     /**
@@ -159,42 +101,10 @@ export class Wallet {
 
     /**
      * @async
-     * Get the Wallet's address 
-     * Adapted from https://developers.cardano.org/docs/get-started/cardano-serialization-lib/generating-keys/
+     * Get the Wallet's address
      */
     async getAddress(): Promise<CSL.Address> {
-        switch (this.method) {
-            case WalletType.Mnemonic: {
-                const paymentCred = CSL.Credential.from_keyhash(this.accountPubKey.to_raw_key().hash());
-                let netId: number = 0;
-                switch (this.network) {
-                    case "mainnet": {
-                        netId = CSL.NetworkInfo.mainnet().network_id();
-                        break;
-                    };
-                    case "preprod": {
-                        netId = CSL.NetworkInfo.testnet_preprod().network_id();
-                        break;
-                    };
-                    case "preview": {
-                        netId = CSL.NetworkInfo.testnet_preview().network_id();
-                        break;
-                    };
-                };
-                // cardano-serialization-lib does not support base addresses without staking credentials.
-                // This is required when initialising the wallet with email
-                // I'll create an Enterprise address instead for now.
-                const baseAddr = CSL.EnterpriseAddress.new(
-                    netId,
-                    paymentCred,
-                );
-
-                return baseAddr.to_address()
-            };
-            case WalletType.Google: {
-                return await this.addressForGmail(this.userId);
-            };
-        }
+        return await this.addressForGmail(this.userId);
     }
 
     /**
@@ -284,107 +194,19 @@ export class Wallet {
     }
 
     /**
-     * Build a transaction to send funds from a sender to the recipient.
-     * Works only for transactions between Cardano addresses.
-     *
-     * @async
-     * @param {CSL.Address} senderAddress    - A Cardano address of the sender
-     * @param {CSL.Address} recipientAddress - A Cardano address of the recipient
-     * @param {CSL.Address} recipientAddress - A Cardano address of the recipient
-     */
-    private async buildTx(senderAddress: CSL.Address, recipientAddress: CSL.Address, assets: Asset): Promise<CSL.TransactionBuilder> {
-        const utxos = await this.getUtxos();
-
-        const txBuilderCfg =
-            CSL.TransactionBuilderConfigBuilder.new()
-                .fee_algo(
-                    CSL.LinearFee.new(
-                        CSL.BigNum.from_str("44"),
-                        CSL.BigNum.from_str("155381")
-                    )
-                )
-                .coins_per_utxo_byte(CSL.BigNum.from_str("4310"))
-                .pool_deposit(CSL.BigNum.from_str("500000000"))
-                .key_deposit(CSL.BigNum.from_str("2000000"))
-                .max_value_size(5000)
-                .max_tx_size(16384)
-                .prefer_pure_change(true)
-                .ex_unit_prices(CSL.ExUnitPrices.new(
-                    CSL.UnitInterval.new(
-                        CSL.BigNum.from_str("577"),
-                        CSL.BigNum.from_str("10000")
-                    ),
-                    CSL.UnitInterval.new(
-                        CSL.BigNum.from_str("721"),
-                        CSL.BigNum.from_str("10000000")
-                    )
-                ))
-                .build();
-
-        const txBuilder = CSL.TransactionBuilder.new(txBuilderCfg);
-
-        const txInputBuilder = CSL.TxInputsBuilder.new();
-
-        utxos.forEach((utxo) => {
-            if (utxo.value['lovelace'] != null) {
-                const ada = utxo.value['lovelace'];
-                const hash = CSL.TransactionHash.from_bytes(BufferUtils.from(utxo.ref.transaction_id, "hex"))
-                const input = CSL.TransactionInput.new(hash, utxo.ref.output_index);
-                const value = CSL.Value.new(ada.toBigNum());
-                const addr = utxo.address;
-                txInputBuilder.add_regular_input(addr, input, value);
-            }
-        });
-        txBuilder.set_inputs(txInputBuilder);
-
-        for (const [key, value] of Object.entries(assets)) {
-            if (key == 'lovelace') {
-                const output = CSL.TransactionOutput.new(
-                    recipientAddress,
-                    CSL.Value.new(value.toBigNum()),
-                );
-                txBuilder.add_output(output);
-            } else {
-                const parts = key.split(".");
-                const policyId = parts[0];
-                const assetName = parts[1];
-                const assets = CSL.Assets.new();
-                assets.insert(
-                    CSL.AssetName.new(hexToBytes(assetName)),
-                    value.toBigNum(),
-                );
-                const masset = CSL.MultiAsset.new();
-                masset.insert(CSL.ScriptHash.from_hex(policyId), assets);
-                const output = CSL.TransactionOutput.new(
-                    recipientAddress,
-                    CSL.Value.new_with_assets(value.toBigNum(), masset),
-                );
-                txBuilder.add_output(output);
-            }
-        }
-
-        txBuilder.add_change_if_needed(senderAddress);
-
-        return txBuilder;
-    }
-
-    /**
      * Send funds from this wallet to a recipient.
      *
      * @async
      * @param {SmartTxRecipient} rec - A recipient with a Cardano or a gmail address
      */
     async sendTo(rec: SmartTxRecipient): Promise<string> {
-        console.log(this.method);
         console.log(rec.recipientType);
         console.log(rec.address);
         console.log(rec.assets);
 
-
-        const senderAddress = await this.getAddress();
         let recipientAddress;
 
-        if (rec.recipientType == WalletType.Google) {
+        if (rec.recipientType == AddressType.Google) {
             recipientAddress = await this.addressForGmail(rec.address);
         } else {
             recipientAddress = CSL.Address.from_bech32(rec.address);
@@ -392,66 +214,41 @@ export class Wallet {
 
         // Prepare email recipients list
         const emailRecipients: string[] = [];
-        if (rec.recipientType == WalletType.Google) {
+        if (rec.recipientType == AddressType.Google) {
             emailRecipients.push(rec.address);
         }
 
-        switch (this.method) {
-            case WalletType.Mnemonic: {
-                // A classical transaction from an address behind a private key to another address or a smart contract
-                const txBuilder = await this.buildTx(senderAddress, recipientAddress, rec.assets);
+        let txHex;
+        const outs: Output[] = [{ address: recipientAddress.to_bech32(), value: rec.assets }];
 
-                const txBody = txBuilder.build();
+        if (!this.freshKey) {
+            const resp = await this.backend.sendFunds(this.userId, outs, this.tokenSKey.to_public().to_raw_key().hash().to_hex());
+            txHex = resp.transaction;
+        } else {
+            const pubkeyHex = this.tokenSKey.to_public().to_raw_key().hash().to_hex();
+            const parts = this.jwt.split(".");
+            const header = atob(parts[0].replace(/-/g, '+').replace(/_/g, '/'));
+            const payload = atob(parts[1].replace(/-/g, '+').replace(/_/g, '/'));
 
-                const transaction = CSL.FixedTransaction.new_from_body_bytes(txBody.to_bytes());
-                transaction.sign_and_add_vkey_signature(this.accountPrvKey.derive(0).derive(0).to_raw_key());
-
-                const signedTxHex = Array.from(new Uint8Array(transaction.to_bytes())).map(b => b.toString(16).padStart(2, '0')).join('');
-                return await this.backend.submitTx(signedTxHex, emailRecipients);
+            const keyId = JSON.parse(header).kid;
+            const matchingKey = await getMatchingKey(keyId);
+            const signature = parts[2].replace(/-/g, '+').replace(/_/g, '/');
+            const empi = {
+                piPubE: b64ToBn(matchingKey.e.replace(/-/g, '+').replace(/_/g, '/')),
+                piPubN: b64ToBn(matchingKey.n.replace(/-/g, '+').replace(/_/g, '/')),
+                piSignature: b64ToBn(signature),
+                piTokenName: new BigIntWrap("0x" + pubkeyHex)
             };
 
-            case WalletType.Google: {
-                // A transaction from a Web2-initialised wallet to any kind of address
-                const is_initialised = await this.backend.isWalletInitialised(this.userId, this.tokenSKey.to_public().to_raw_key().hash().to_hex());
-                console.log(`Is initialised: ${is_initialised}`);
-                let txHex;
+            const proofBytes = await this.prover.prove(empi);
+            const resp = await this.backend.createAndSendFunds(this.userId, header + '.' + payload, pubkeyHex, proofBytes, outs);
+            txHex = resp.transaction;
+        }
+        const transaction = CSL.FixedTransaction.from_bytes(hexToBytes(txHex));
+        transaction.sign_and_add_vkey_signature(this.tokenSKey.to_raw_key());
+        const signedTxHex = Array.from(new Uint8Array(transaction.to_bytes())).map(b => b.toString(16).padStart(2, '0')).join('');
 
-                const outs: Output[] = [{ address: recipientAddress.to_bech32(), value: rec.assets }];
-
-                if (is_initialised && !this.freshKey) {
-                    const resp = await this.backend.sendFunds(this.userId, outs, this.tokenSKey.to_public().to_raw_key().hash().to_hex());
-                    txHex = resp.transaction;
-                } else {
-                    const pubkeyHex = this.tokenSKey.to_public().to_raw_key().hash().to_hex();
-                    console.log(pubkeyHex);
-                    const parts = this.jwt.split(".");
-                    const header = atob(parts[0].replace(/-/g, '+').replace(/_/g, '/'));
-                    const payload = atob(parts[1].replace(/-/g, '+').replace(/_/g, '/'));
-
-                    const keyId = JSON.parse(header).kid;
-                    const matchingKey = await getMatchingKey(keyId);
-                    const signature = parts[2].replace(/-/g, '+').replace(/_/g, '/');
-                    const empi = {
-                        piPubE: b64ToBn(matchingKey.e.replace(/-/g, '+').replace(/_/g, '/')),
-                        piPubN: b64ToBn(matchingKey.n.replace(/-/g, '+').replace(/_/g, '/')),
-                        piSignature: b64ToBn(signature),
-                        piTokenName: new BigIntWrap("0x" + pubkeyHex)
-                    };
-
-                    //const instance = await initialiseWASI();
-                    //const proofBytes = mkProofBytesMock(instance, 0n, new Array(19).fill(0n), empi);
-                    const proofBytes = await this.prover.prove(empi);
-
-                    const resp = await this.backend.createAndSendFunds(this.userId, header + '.' + payload, pubkeyHex, proofBytes, outs);
-                    txHex = resp.transaction;
-                }
-                const transaction = CSL.FixedTransaction.from_bytes(hexToBytes(txHex));
-                transaction.sign_and_add_vkey_signature(this.tokenSKey.to_raw_key());
-                const signedTxHex = Array.from(new Uint8Array(transaction.to_bytes())).map(b => b.toString(16).padStart(2, '0')).join('');
-
-                return await this.backend.submitTx(signedTxHex, emailRecipients);
-            };
-        };
+        return await this.backend.submitTx(signedTxHex, emailRecipients);
     }
 
 }
