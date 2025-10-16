7655c407a303ac08f4d4c752340aff673106374a
Vladimir Sinyakov <vladimir.siniakov@gmail.com>
2025-10-16 15:43:00 +0200

    Update Utils.ts

--- src/Utils.ts
+++ src/Utils.ts
@@ -5,38 +5,38 @@ import { BigIntWrap } from "./Types";
  * https://stackoverflow.com/questions/14603205/how-to-convert-hex-string-into-a-bytes-array-and-a-bytes-array-in-the-hex-strin
  */
 export function hexToBytes(hex: string): Uint8Array {
-    const bytes = [];
+    const bytes = []
     for (let c = 0; c < hex.length; c += 2)
-        bytes.push(parseInt(hex.substr(c, 2), 16));
-    return Uint8Array.from(bytes);
+        bytes.push(parseInt(hex.substr(c, 2), 16))
+    return Uint8Array.from(bytes)
 }
 
 /**
  * Convert bytes to hex string
  */
 export function bytesToHex(bytes: Uint8Array): string {
-    return Array.from(bytes).map(b => b.toString(16).padStart(2, '0')).join('');
+    return Array.from(bytes).map(b => b.toString(16).padStart(2, '0')).join('')
 }
 
 /**
  * Convert BigInt to byte array
  */
 export function bigIntToBytes(bigInt: bigint): Uint8Array {
-    const hex = bigInt.toString(16);
-    const paddedHex = hex.length % 2 ? '0' + hex : hex;
-    const bytes = new Uint8Array(paddedHex.length / 2);
+    const hex = bigInt.toString(16)
+    const paddedHex = hex.length % 2 ? '0' + hex : hex
+    const bytes = new Uint8Array(paddedHex.length / 2)
     for (let i = 0; i < paddedHex.length; i += 2) {
-        bytes[i / 2] = parseInt(paddedHex.substr(i, 2), 16);
+        bytes[i / 2] = parseInt(paddedHex.substr(i, 2), 16)
     }
-    return bytes;
+    return bytes
 }
 
 /**
  * Convert bytes to base64url string
  */
 export function bytesToBase64Url(bytes: Uint8Array): string {
-    const base64 = btoa(String.fromCharCode(...bytes));
-    return base64.replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
+    const base64 = btoa(String.fromCharCode(...bytes))
+    return base64.replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '')
 }
 
 export function harden(num: number): number {
