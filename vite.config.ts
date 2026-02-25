90b77c3127995d0fe5af366133d45f7c1ce5e108
Vladimir <me.morozovvp@gmail.com>
2026-02-25 17:11:22 +1000

    Fixed vulnerabilities

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import dts from 'vite-plugin-dts'
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
-import inject from '@rollup/plugin-inject'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
 
 export default defineConfig({
   plugins: [
@@ -21,6 +22,45 @@ export default defineConfig({
           },
         }),
   ],
+  resolve: {
+      alias: {
+          // This Rollup aliases are extracted from @esbuild-plugins/node-modules-polyfill, 
+          // see https://github.com/remorses/esbuild-plugins/blob/master/node-modules-polyfill/src/polyfills.ts
+          util: 'rollup-plugin-node-polyfills/polyfills/util',
+          sys: 'util',
+          events: 'rollup-plugin-node-polyfills/polyfills/events',
+          stream: 'rollup-plugin-node-polyfills/polyfills/stream',
+          path: 'rollup-plugin-node-polyfills/polyfills/path',
+          querystring: 'rollup-plugin-node-polyfills/polyfills/qs',
+          punycode: 'rollup-plugin-node-polyfills/polyfills/punycode',
+          url: 'rollup-plugin-node-polyfills/polyfills/url',
+          string_decoder:
+              'rollup-plugin-node-polyfills/polyfills/string-decoder',
+          http: 'rollup-plugin-node-polyfills/polyfills/http',
+          https: 'rollup-plugin-node-polyfills/polyfills/http',
+          os: 'rollup-plugin-node-polyfills/polyfills/os',
+          assert: 'rollup-plugin-node-polyfills/polyfills/assert',
+          constants: 'rollup-plugin-node-polyfills/polyfills/constants',
+          _stream_duplex:
+              'rollup-plugin-node-polyfills/polyfills/readable-stream/duplex',
+          _stream_passthrough:
+              'rollup-plugin-node-polyfills/polyfills/readable-stream/passthrough',
+          _stream_readable:
+              'rollup-plugin-node-polyfills/polyfills/readable-stream/readable',
+          _stream_writable:
+              'rollup-plugin-node-polyfills/polyfills/readable-stream/writable',
+          _stream_transform:
+              'rollup-plugin-node-polyfills/polyfills/readable-stream/transform',
+          timers: 'rollup-plugin-node-polyfills/polyfills/timers',
+          console: 'rollup-plugin-node-polyfills/polyfills/console',
+          vm: 'rollup-plugin-node-polyfills/polyfills/vm',
+          zlib: 'rollup-plugin-node-polyfills/polyfills/zlib',
+          tty: 'rollup-plugin-node-polyfills/polyfills/tty',
+          domain: 'rollup-plugin-node-polyfills/polyfills/domain',
+          buffer: 'rollup-plugin-node-polyfills/polyfills/buffer-es6',
+          process: 'rollup-plugin-node-polyfills/polyfills/process-es6'
+      }
+  },
   build: {
     lib: {
       entry: resolve(__dirname, 'index.ts'),
@@ -46,6 +86,20 @@ export default defineConfig({
     global: 'globalThis'
   },
   optimizeDeps: {
-    include: ['@scure/bip39', 'axios', 'json-bigint']
+    include: ['@scure/bip39', 'axios', 'json-bigint'],
+    esbuildOptions: {
+        // Node.js global to browser globalThis
+        define: {
+            global: 'globalThis'
+        },
+        // Enable esbuild polyfill plugins
+        plugins: [
+            NodeGlobalsPolyfillPlugin({
+                process: true,
+                buffer: true
+            }),
+            NodeModulesPolyfillPlugin()
+        ]
+    }
   }
 })
